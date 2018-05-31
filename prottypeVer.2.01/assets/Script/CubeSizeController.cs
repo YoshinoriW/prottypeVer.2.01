ffe19756a604a089b1a034d42fe6a817834167e6
watanabeyoshinori <wyoshinori.atpg@outlook.jp>
2018-05-31 21:06:41 +0900

    自作ゲームのプロトタイプ
    前回とほぼ変わらず床の作成とオブジェクトの拡大縮小がまだ途中です

---
+++ prottypeVer.2.01/assets/Script/CubeSizeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    //拡大処理、試験的にとりあえず+1
+    int plusHeight = 1;
+    //ゲームオブジェクト
+    private GameObject GCube;
+    //サイズとポジション
+    private Vector3 startlcs;
+    private Vector3 startpos;
+
+    // Use this for initialization
+    void Start () {
+        //緑キューブのオブジェクト取得
+        this.GCube = GameObject.Find("GCube");
+        //緑キューブの初期座標保存。必ずサイズ情報、初期位置の2つ取る。
+        startlcs = this.GCube.transform.localScale;
+        startpos = this.GCube.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ボタンクリック時
+        if (Input.GetMouseButtonDown(0))
+        {
+            //オブジェクトのローカルサイズ変更
+            //new Vector3(x軸,y軸,z軸)
+            transform.localScale += new Vector3(plusHeight, plusHeight, plusHeight);
+            //y軸上下に+1されてしまうので2で割る。
+            transform.position += new Vector3(0, plusHeight / 2f, 0);
+
+        }
+        //ボタンを離した時
+        if (Input.GetMouseButtonUp(0))
+        {
+            //サイズと座標を初期値に戻す。片方だけ戻すと座標がずれる。
+            transform.localScale = startlcs;
+            transform.position = startpos;
+            
+        }
+    }
+}
---
+++ prottypeVer.2.01/assets/Script/LensScript/LensCollisionAction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LensCollisionAction : MonoBehaviour
+{
+
+    //拡大処理、試験的にとりあえず+1
+    int plusHeight = 1;
+
+    //ゲームオブジェクト
+    private GameObject Sphere;
+
+    //サイズとポジションの初期座標保存。必ずサイズ情報、初期位置の2つ取る。
+    private Vector3 startlcs;
+    private Vector3 startpos;
+
+
+    void Start()
+    {
+        //カプセルのオブジェクト取得
+        this.Sphere = GameObject.Find("SphereTag");
+        //テストキューブの初期座標保存。必ずサイズ情報、初期位置の2つ取る。
+        startlcs = this.Sphere.transform.localScale;
+        startpos = this.Sphere.transform.position;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "LensTag")
+        {
+            //オブジェクトのローカルサイズ変更
+            //new Vector3(x軸,y軸,z軸)
+            //Debug.Log("当たったよ");
+            this.transform.localScale += new Vector3(plusHeight, plusHeight, plusHeight);
+
+            //y軸上下に+1されてしまうので2で割る。
+            this.transform.position += new Vector3(0, plusHeight / 2f, 0);
+
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //サイズと座標を初期値に戻す。片方だけ戻すと座標がずれる。
+        transform.localScale = startlcs;
+        transform.position = startpos;
+
+    }
+}
